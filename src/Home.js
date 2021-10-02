6e47c0c1b878959b2c9a30247e46718eea84830c
federicodonner <federico.donner@gmail.com>
2021-10-01 22:56:17 -0300

    fix(Home): remove fab logo

--- src/Home.js
+++ src/Home.js
@@ -3,7 +3,9 @@ export default function Home() {
   return (
     <div className="mainRoute home">
       <div className="fabLogoContainer">
-        <img src={fabLogo} className="fabLogo" alt="logo" />
+        <div className="fabstyle">FLESH</div>
+        <div className="fabstyle and">AND</div>
+        <div className="fabstyle">BLOOD</div>
       </div>
       <div className="title">Penalty Guidelines</div>
       <div className="subtitle">Version 1.7 (2021-09-24)</div>
